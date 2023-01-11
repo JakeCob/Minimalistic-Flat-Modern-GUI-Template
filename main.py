5c2180dce4ca77e740a6cd5b636d7c1c20ac9944
JakeCob <rafaljacobmatthew@gmail.com>
2023-01-11 18:02:19 +0800

    fixed ui of main

--- main.py
+++ main.py
@@ -1,35 +1,3 @@
-
-###########################################################################################
-###                        CODE:       WRITTEN BY: ANJAL.P AUGUST 11 2020               ###
-###                        PROJECT:    PELLIS Z1                                        ###
-###                        PURPOSE:    WINDOWS/LINUX/MAC OS FLAT MODERN UI              ###
-###                                    BASED ON QT DESIGNER, PySide2                    ###
-###                        USE CASE:   TEMPLATE FOR SOFTWARES                           ###
-###                        LICENCE:    MIT OPENSOURCE LICENCE                           ###
-###                                                                                     ###
-###                            CODE IS FREE TO USE AND MODIFY                           ###
-###########################################################################################
-
-###########################################################################################
-#                                     DOCUMENTATION                                       #
-#                                                                                         #
-#  Each line of the code described below is commented well, such as: the purpose of the   #
-#  code, its function, returns e.t.c as in certain case: the alternatives to that solul-  #
-#  ution, other sources like included PDF document has also the working of the code.      #
-#  CSS stylesheet of the buttons are given seperatly in the CSS.txt in the parent folder  #
-###########################################################################################
-
-###########################################################################################
-#                                       CAUTION                                           #
-#  SINCE MOST OF THE WORK IS DONE IN THE QT DESIGNER, YOU MAY NOT SEE THE STYLESHEET HERE #
-#  FOR THAT PLEASE REFER THE Documentation.pdf                                            #
-#  ALSO MANY OF THE SETTINGS IS PREDEFINED IN THE QT DESIGNER ITSELF, SO HERE IN THIS FUN-#
-#  CTION WHAY HAPPENS AFTER THIS I.E. WHEN THE USER CHANGES THE INPUT STATE, ONLY IS DELT #
-#  HERE, SO IF YOU WANT TO MODIFY THE FILE, PLEASE OPEN THE CORRESPONDING .ui FILE IN QT  #
-#  DESIGNER AND MADE THE MODIFICATION AND THENY COME BACK HERE TO ADD FUNCTIONALITY TO THE#
-#  CHANGES.                                                                               #
-###########################################################################################    
-
 import sys
 
 #IMPORTING ALL THE NECESSERY PYSIDE2 MODULES FOR OUR APPLICATION.
